dffba6bc88d62e2deb9e9040036dab08904eb7b5
השם שלך < >
2025-02-24 14:17:34 +0200

    Merge remote-tracking branch 'adskipper2/master'

--- app/src/main/java/com/example/adskipper2/MainActivity.kt
+++ app/src/main/java/com/example/adskipper2/MainActivity.kt
@@ -1,22 +1,7 @@
 package com.example.adskipper2
 
 import android.Manifest
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import android.app.Activity
 import android.app.ActivityManager
 import android.content.Intent
 import android.content.SharedPreferences
@@ -47,7 +32,6 @@ import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import java.io.File
-import android.accessibilityservice.AccessibilityServiceInfo
 import android.view.accessibility.AccessibilityManager
 import android.content.Context
 
